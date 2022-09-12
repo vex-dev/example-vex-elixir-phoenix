f12a8e871ba757f7a9b0ce9ebdb2691bfbd239a8
Sam Pierson <sam@geometer.io>
2022-09-12 10:25:51 -0800

    Create a JWT

--- lib/vex_phx_web/controllers/page_controller.ex
+++ lib/vex_phx_web/controllers/page_controller.ex
@@ -2,6 +2,24 @@ defmodule VexDemoWeb.PageController do
   use VexDemoWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    vex_api_key_id = System.get_env("VEX_API_KEY_ID")
+    vex_api_secret = System.get_env("VEX_API_SECRET")
+    room_id = System.get_env("VEX_ROOM_ID")
+
+    claims = %{
+      "action" => "join",
+      "room_id" => room_id
+    }
+
+    signer =
+      Joken.Signer.create(
+        "HS256",
+        vex_api_secret,
+        %{"api_key_id" => vex_api_key_id}
+      )
+
+    {:ok, jwt} = Joken.Signer.sign(claims, signer)
+
+    render(conn, "index.html", jwt: jwt, room_id: room_id)
   end
 end
